d3fc638dbd05eb579020236ff2387d7688442752
Ashray Deep Bajpai <ashraydeepbajpai77@gmail.com>
2024-03-12 14:20:47 +0530

    MovieZilla Completed

---
+++ src/pages/4O4/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      Page Not Found
+    </div>
+  )
+}
+import './style.scss'
+
+export default PageNotFound
